561a84cea0f6ac7afd18fac5c08b1bc97752f79c
Clement Auger <clement.ager@d1m.cn>
2015-10-22 14:14:19 +0800

    add support for form validaton

--- src/templates/formDemo.php
+++ src/templates/formDemo.php
@@ -11,3 +11,19 @@ echo $this->form_start($myForm);
 echo $this->form_rows($myForm);
 echo $this->form_end($myForm);
 ?>
+
+
+<?php $this->inlineTo('last'); ?>
+<script>
+    (function () {
+        var form = $("#myForm");
+        form.ajaxForm({
+            dataType: 'json',
+            success: window.CHelpers.formErrorProcessor(form)
+        });
+        form.on('reset', function () {
+            form.find('.errors').remove()
+        })
+    })();
+</script>
+<?php $this->endInline(); ?>
